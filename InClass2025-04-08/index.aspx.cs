e8bc3df6a6f4a2c1e81da82bda67b8e18de5a6dd
Rhoda Asiamah <asiamaro@mail.uc.edu>
2025-04-08 12:13:09 -0400

    I added more button commands to code

--- InClass2025-04-08/index.aspx.cs
+++ InClass2025-04-08/index.aspx.cs
@@ -11,7 +11,19 @@ namespace InClass2025_04_08
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            cmdOK.Click += cmdOK_OnClick;
+        }
+        protected void cmdOK_OnClick(object sender, EventArgs e)
+        {
+            //call your .cs file code here
+            lblResult.Text = "You clicked!";
 
+            DateTime date1 = calCheckInDate.SelectedDate;
+            DateTime date2 = calCheckInDate.SelectedDate;
+
+            int daysDifference = (date2-date1).Days;
+
+            lblResult.Text = "Total: " + ((daysDifference * 200) + ((daysDifference * 200) * 0.15));
         }
     }
 }
